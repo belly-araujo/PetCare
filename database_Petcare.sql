23dd881a0dc1a8c6dbeebac7d9accb79ff1d916e
Isabelly Araújo <isabelly.araujo0608@gmail.com>
2025-11-12 19:24:50 -0300

    Add files via upload
    Adicionei o script do banco de dados da petcare

---
+++ database_Petcare.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS petcare; 
+
+CREATE TABLE pet (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  especie VARCHAR(50),
+  idade INT,
+  tutor VARCHAR(100)
+);
